ad6c79a7971594e789edc2635a36f18ac75a81c4
J-Harle <158578157+J-Harle@users.noreply.github.com>
2024-05-03 18:09:43 +0100

    Update 4Body1.0.py

--- 4Body1.0.py
+++ 4Body1.0.py
@@ -1,10 +1,8 @@
-
 from collections import defaultdict
 import itertools
 
 def UserInputs():
-#    k = int(input("How many amino acids are in the structure?: "))
-     N = int(input("How many amino acids would you like to include in the interaction?: "))
+    N = int(input("How many amino acids would you like to include in the interaction?: "))
     UnCorrFileName = input("Which file would you like to open? (Do not include .com): ")
     FileName = UnCorrFileName + ".com"
     Mem = str(int(input("How much memory would you like to use (In GB)?: ")))
@@ -12,14 +10,9 @@ def UserInputs():
     Functional = input("Which functional would you like to use?: ")
     BasisSet = input("Which basis set would you like to use?: ")
     Solvent = input("Which solvent would you like to use? If none, leave blank: ")
-    CorrSolvent = "SCRF=(Solvent=" + Solvent + ")"
-    Solvent2 = None
-    if Solvent == "None" or Solvent == "none" or Solvent == "":
-        CorrSolvent = Solvent2
-    else:
-        CorrSolvent = CorrSolvent
+    CorrSolvent = f"SCRF=(Solvent={Solvent})" if Solvent.lower() != "none" else ""
     OtherInput = input("Would you like any other commands in the command line? If none, leave blank: ")
-    return k, N, FileName, Mem, Cores, Functional, BasisSet, Solvent
+    return N, FileName, Mem, Cores, Functional, BasisSet, CorrSolvent
 
 def ReadingFile(FileName, k):
     ligand = []
@@ -34,32 +27,24 @@ def ReadingFile(FileName, k):
                 if fragment in line:
                     fragment_list[i - 1].append(line)
 
-    # Create copies of ligand a fragment lists then line.replace
-    bq_ligand = ligand.copy()
-    blank_ligand = ligand.copy()
-    bq_fragment_list = fragment_list.copy()
-    blank_fragment_list = fragment_list.copy()
-    
-    for i in range(1, k+3):
-        bq_ligand = [line.replace("(Fragment=" + str(i) + ")", "-Bq") for line in bq_ligand]
-        bq_ligand = [line.replace("(Fragment=" + str(i) + ")", "-Bq") for line in ligand]
-        blank_ligand = [line.replace("(Fragment=" + str(i) + ")", " ") for line in ligand]
-        bq_fragment_list[i - 1] += [line.replace("(Fragment=" + str(i) + ")", "-Bq") for line in fragment_list[i - 1]]
-        blank_fragment_list[i - 1] += [line.replace("(Fragment=" + str(i) + ")", " ") for line in fragment_list[i - 1]]
+    bq_ligand = [line.replace("(Fragment=" + str(i) + ")", "-Bq") for line in ligand]
+    blank_ligand = [line.replace("(Fragment=" + str(i) + ")", " ") for line in ligand]
+    bq_fragment_list = {key: [line.replace("(Fragment=" + str(key) + ")", "-Bq") for line in fragment_list[key]] for key in fragment_list}
+    blank_fragment_list = {key: [line.replace("(Fragment=" + str(key) + ")", " ") for line in fragment_list[key]] for key in fragment_list}
 
     return ligand, bq_ligand, blank_ligand, fragment_list, bq_fragment_list, blank_fragment_list
 
-def generate_interactions(Fragments, K):
+def generate_interactions(Fragments, N):
     # Generate all combinations with the desired length
-    combos = itertools.combinations(Fragments, K)
+    combos = itertools.combinations(Fragments, N)
     
     # Initialize sets to store unique interactions
-    FourBodyInteractions = []
-    ThreeBodyInteractions = []
-    PairwiseInteractions = []
+    FourBodyInteractions = set()
+    ThreeBodyInteractions = set()
+    PairwiseInteractions = set()
     
     for combo in combos:
-        #This is hard coded to generate 4 body interactions, but is easily scable
+        # This is hard coded to generate 4 body interactions, but is easily scalable
         four_body_combos = list(itertools.combinations(combo, N))
         FourBodyInteractions.update(four_body_combos)
         
@@ -76,10 +61,10 @@ def generate_interactions(Fragments, K):
 
 
 def Headers(FragmentCombinations, ligand, user_inputs, bq_ligand, blank_ligand, bq_fragment_list, blank_fragment_list):
-    k, N, FileName, Mem, Cores, Functional, BasisSet, CorrSolvent = user_inputs
+    N, FileName, Mem, Cores, Functional, BasisSet, CorrSolvent = user_inputs
     counter = 1  
     for combo in FragmentCombinations:
-        OutputFile = str(counter) + "-New.com"  
+        OutputFile = f"{counter}-New.com"
         with open(OutputFile, "w") as outputfile: 
             FragmentCharge = 0 
             LigandCharge = 0    
@@ -89,14 +74,14 @@ def Headers(FragmentCombinations, ligand, user_inputs, bq_ligand, blank_ligand, 
             TotalMultiplicity = max(FragmentMultiplicity, LigandMultiplicity)
             
             BaseHeader = [
-            f"%chk={counter}-New.chk\n",
-            f"%mem={Mem}GB\n",
-            f"%nprocshared={Cores}\n",
-            f"#{Functional} {BasisSet} {CorrSolvent}\n",
-            "\n",
-            "Josh Harle - MSc Project Code\n",
-            "\n"
-        ]
+                f"%chk={counter}-New.chk\n",
+                f"%mem={Mem}GB\n",
+                f"%nprocshared={Cores}\n",
+                f"#{Functional} {BasisSet} {CorrSolvent}\n",
+                "\n",
+                "Josh Harle - MSc Project Code\n",
+                "\n"
+            ]
             TotalHeader = BaseHeader + [f"{TotalCharge} {TotalMultiplicity}\n"]
 
             LigandHeader = BaseHeader + [f"{LigandCharge} {LigandMultiplicity}\n"]
@@ -104,11 +89,10 @@ def Headers(FragmentCombinations, ligand, user_inputs, bq_ligand, blank_ligand, 
             FragmentHeader = BaseHeader + [f"{FragmentCharge} {FragmentMultiplicity}\n"]
             
             w = outputfile.writelines
-            
-                counter += 1
-       
+            counter += 1
+
 user_inputs = UserInputs()
-k, N, FileName, Mem, Cores, Functional, BasisSet, CorrSolvent = user_inputs
+N, FileName, Mem, Cores, Functional, BasisSet, CorrSolvent = user_inputs
 ligand, bq_ligand, blank_ligand, fragment_list, bq_fragment_list, blank_fragment_list = ReadingFile(FileName, k)
-FragmentCombinations = GenerateCombinations(fragment_list.keys(), N)
+FragmentCombinations = generate_interactions(fragment_list.keys(), N)
 Headers(FragmentCombinations, ligand, user_inputs, bq_ligand, blank_ligand, bq_fragment_list, blank_fragment_list)
