f89cccce42aab95c25814159eb82a51b9fd034a2
J-Harle <158578157+J-Harle@users.noreply.github.com>
2024-06-23 13:15:46 +0100

    Update Aminoacids.py

--- Aminoacids.py
+++ Aminoacids.py
@@ -1,9 +1,19 @@
-# Dictionary mapping specific coordinates to their corresponding amino acids and labels
+# Coordinates of Alpha-Carbons for each of the amino acids
 coordinate_to_amino_acid = {
-    "-5.83097600   -0.87369600   -6.96591900 L": "Ala1",
-    "-6.16731500   -1.00122300   -5.55078900 L": "Ala2",
-    "-1.77912300    6.17102700   -1.47821900 L": "Cys2",
-    # Add more coordinates and corresponding amino acids here
+    "-6.1673150    -1.0012230   -5.5507890 L": "Gly",
+    "-10.7245130   -2.3717130   -1.7466690 L": "Thr",
+    "-8.5798880    -5.5231540   0.7363670 L": "Ala",
+    "-1.5104980    -5.3861860   1.4109020 L": "Val",
+    "-4.9314660    -6.4725550   0.1640280 L": "Ser",
+    "-1.7311330     7.7003060   -1.2719070L": "Phe",
+    "-6.1747680 4.7063980 -2.2484910 L": "Val 2",
+    "-9.9421730 2.8696930 2.2321750 L": "Trp",
+    "4.5284160 -4.7614300 1.6830450 L": "Gly2",
+    "5.0647790  0.5872610 6.4829920 L": "Ile",
+    "8.4764910 2.2820350 -2.4158850 L": "Tyr",
+    "4.9069360 4.7402680 3.1808670 L": "Cys",
+    "11.1648970 -2.3408500 -2.3366500 L": "His",
+    "9.9633070 -2.3290650 1.2717110 L": "Gly 3",    
 }
 
 # Function to search for specific coordinates in a file
