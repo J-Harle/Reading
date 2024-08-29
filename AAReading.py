d89300788c82183cd274cae01de843f5c5c14b8b
J-Harle <158578157+J-Harle@users.noreply.github.com>
2024-08-29 16:18:33 +0100

    Update AAReading.py

--- AAReading.py
+++ AAReading.py
@@ -2,7 +2,7 @@
 alpha_c_coord = {
     "C     0   -6.16731500   -1.00122300   -5.55078900 L": "Gly",
     "C     0  -10.72451300   -2.37171300   -1.74666900 L": "Thr",
-    "C                 -8.57988800   -5.52315400    0.73636700": "Ala",
+    "C     0   -8.53063600    1.37360000   -5.27666900 L": "Ala",
     "C    0   -1.51049800   -5.38618600    1.41090200 L": "Val",
     "C    0   -4.93146600   -6.47255500    0.16402800 L": "Ser",
     "C     0   -1.73113300    7.70030600   -1.27190700 L": "Phe",
@@ -15,9 +15,6 @@ alpha_c_coord = {
     "C     0   11.16489700   -2.34085000   -2.33665000 L": "His",
     "C     0    9.96330700   -2.32906500    1.27171100 L": "Gly3",
 }
-# Number of files to process, change accordingly
-num_files = 364  
-
 # Open a text file to write the results
 with open("amino_acids_in_files.txt", "w") as output_file:
     for counter in range(1, num_files + 1):
